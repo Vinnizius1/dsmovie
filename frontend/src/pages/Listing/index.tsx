62f8be0719a33dfafb730f0e7ecf2e2c1b51f531
vinicius <vmm_7@hotmail.com>
2022-05-10 16:35:36 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Pagination from "components/Pagination";
 
 function Listing() {
-  return <div>Página de listagem</div>;
+  return <Pagination />;
 }
 
 export default Listing;
